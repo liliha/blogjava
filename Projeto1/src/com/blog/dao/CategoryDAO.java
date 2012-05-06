c409f2e3a19be10a5d6bf2eff9ebeb5af644292d
Pollyane Carvalho <pollyanecs@gmail.com>
2012-05-06 17:40:38 -0300

    Diff CategoryDAO

--- Projeto1/src/com/blog/dao/CategoryDAO.java
+++ Projeto1/src/com/blog/dao/CategoryDAO.java
@@ -1,29 +1,5 @@
 package com.blog.dao;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 public class CategoryDAO {
 
-	   private EntityManagerFactory emf; 
-
-       /* se vc já está acostumado com Hibernate
-        * a ideia é a mesma do método de fabrica que
-        * criamos para os Session que vem do SessionFactory
-        * Aqui temos apenas nomeclatura diferente, pense assim.
-        */
-       public EntityManager getEntityManager(){
-
-               //responsavel pela persistencia
-               return emf.createEntityManager();
-       }
-       public void DAO() {
-               //chamo ele de reprodutor da instância
-               emf = Persistence.createEntityManagerFactory("javablog");
-
-               /* o nome passado vem do arquivo persistence.xml que contém as configs
-                * para conexão com o BD.
-                */
-       }
 }
